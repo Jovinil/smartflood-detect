4ebdba1551f815359a312f37339758bf226ac6ca
Jovinil <jovinilavila123@gmail.com>
2025-08-20 15:39:54 +0800

    env example added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 
- 
-
   dir: {
     pages: './app/pages',
     layouts: './app/layouts',
+  },
+  imports: {
+    dirs: [
+      'app/composables',          // Top-level composables in app folder
+      'app/composables/**'        // All nested files within composables
+    ]
   },
   components: [
     {
@@ -33,11 +37,37 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
     'nuxt-mapbox',
+    'nuxt-vuefire',
   ],
+  
+  vuefire: {
+    config: {
+      apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+      appId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+      storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
+
+      // there could be other properties depending on the project
+    },
+
+    auth: {
+      enabled: true
+    },
+  },
+
   
    runtimeConfig: {
     public: {
-      mapboxToken: process.env.NUXT_PUBLIC_MAPBOX_TOKEN
+      mapboxToken: process.env.NUXT_PUBLIC_MAPBOX_TOKEN,
+      // firebaseApiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+      // firebaseAuthDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      // firebaseProjectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+      // firebaseStorageBucket:  process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      // firebaseMessagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      // firebaseAppId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+      // firebaseMeasurementId: process.env.NUXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
     }
   },
   mapbox: {
@@ -45,5 +75,7 @@ export default defineNuxtConfig({
     accessToken: process.env.NUXT_PUBLIC_MAPBOX_TOKEN
   },
 
-  colorMode: { classSuffix: '' }
+  colorMode: { classSuffix: '' },
+
+  plugins: ['~/plugins/firebase.ts']
 });
