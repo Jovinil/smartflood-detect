ed6ffe249aba1bf8f8bdd49b5311845dae4b0fcc
Jovinil <jovinilavila123@gmail.com>
2025-08-26 14:04:40 +0800

    added admin UI

---
+++ app/pages/admin/profile/editing-profile.vue
@@ -0,0 +1,54 @@
+<template>
+    <!-- <div class="flex justify-center gap-2">
+        <UButton color="primary" variant="outline" to="/admin/registration/otp">Login</UButton>
+        <UButton color="primary" variant="outline" to="/admin/registration/otp">Login</UButton>
+        
+    </div> -->
+   <h1 class="text-2xl font-bold mb-4 text-center">Login</h1>
+   <div>
+        <p class="text-[12px] text-gray-700 dark:text-gray-400 mb-2">Username</p>
+            <UInput 
+            color="neutral" 
+            variant="subtle" 
+            icon="i-lucide-user" 
+            placeholder="Username" 
+            type="text"
+            class="mb-3 w-full"
+        />
+
+   </div>
+   <div>
+        <p class="text-[12px] text-gray-700 dark:text-gray-400 mb-2">Password</p>
+        <UInput 
+        color="neutral" 
+        variant="subtle" 
+        icon="i-lucide-lock" 
+        placeholder="Password" 
+        type="password"
+        class="mb-3 w-full"
+        />
+
+    </div>
+    <div>
+        <p class="text-[12px] text-gray-700 dark:text-gray-400 mb-2">Confirm Password</p>
+        <UInput 
+        color="neutral" 
+        variant="subtle" 
+        icon="i-lucide-lock" 
+        placeholder="Password" 
+        type="password"
+        class="mb-3 w-full"
+        />
+
+   </div>
+   
+    <div class="text-end w-full">
+      <UButton color="primary" variant="outline" to="/admin/content">Save</UButton>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'login' 
+})
+</script>
