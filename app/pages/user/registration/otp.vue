d70cf7dcc484f8ad3934b3ba8c90d0d04a41859e
Jovinil <jovinilavila123@gmail.com>
2025-09-19 04:09:58 +0800

    added resend code and auto redirect to home page after successfully verifying OTP

--- app/pages/user/registration/otp.vue
+++ app/pages/user/registration/otp.vue
@@ -7,20 +7,38 @@
         <h1 class="text-2xl font-bold mb-4">You're almost there!</h1>
         <p class="text-[16px] text-gray-800 dark:text-gray-200">OTP</p>
         <p class="text-[12px] text-gray-600 dark:text-gray-400 mb-2">Verifying phone: {{ otpStore.phoneNumber }}</p>
+        <div ref="recaptchaDiv"></div>
 
-        <div class="ext-center w-full">
+        <div class="text-center w-full">
           <UPinInput 
             :length="6"
             v-model="otpStore.verificationCode"
           />
+            
+        
           <p v-if="otpStore.errorMessage" class="text-red-500">{{ otpStore.errorMessage }}</p>
         </div>
 
         <div class="text-end mt-3">
-          <div id="recaptcha"></div>
+          <p class="text-sm"> 
+            Didn't receive code?
+            
+            <UButton 
+              @click="otpStore.sendCode()"
+              :disabled="otpStore.isSending"
+              variant="link"
+              color="info"
+              :ui="{ base: 'p-0' }"
+            >
+              {{ otpStore.isSending ? 'Sending...' : 'Resend Code' }}
+            </UButton>
+
+          </p>
+          
           <UButton 
-            @click="otpStore.verifyCode"
+            @click="handleVerifyCode"
             :disabled="otpStore.isVerifying"
+            class="mt-2"
           >
             {{ otpStore.isVerifying ? 'Verifying...' : 'Verify Code' }}
           </UButton>
@@ -40,9 +58,13 @@ definePageMeta({
   layout: 'login' 
 })
 
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { useAuthOtpStore } from '~/app/stores/useAuthOtpStore'
+import { useRouter } from 'vue-router'
 
-const otpStore = useAuthOtpStore()
+const router = useRouter();
+const otpStore = useAuthOtpStore();
+const recaptchaDiv = ref<HTMLElement | null>(null)
 
 const hi = () => {
   console.log("code", otpStore.verificationCodeString)
@@ -54,5 +76,22 @@ watch(() => otpStore.verificationCode, (val) => {
 
 watch(() => otpStore.verificationCodeString, (val) => {
   console.log('verificationCodeString:', val)
+})
+
+const handleVerifyCode = async () => {
+  const success = await otpStore.verifyCode()
+  if (success) {
+    router.push('/user/registration/redirect') // 👈 redirect after success
+  }
+}
+
+
+onMounted(() => {
+  otpStore.recaptchaContainer = recaptchaDiv.value
+  otpStore.initRecaptcha()
+})
+
+onBeforeUnmount(() => {
+  otpStore.clearRecaptcha()
 })
 </script>
--- app/pages/user/registration/redirect.vue
+++ app/pages/user/registration/redirect.vue
@@ -2,11 +2,28 @@
 
     <h1 class="text-2xl font-bold mb-4 text-center">Registration Complete</h1>
     <p class="text-[16px] text-gray-800 dark:text-gray-200 mb-2 text-center">Thank you for registering! You will be redirected to the main page shortly</p>
-    <p class="text-[12px] text-gray-700 dark:text-gray-400 text-center">If not redirected automatically click the   <ULink to="/user/" class="underline text-blue-400">here</ULink></p>
+    <p class="text-[12px] text-gray-700 dark:text-gray-400 text-center">If not redirected automatically click the   <ULink to="/" class="underline text-blue-400">here</ULink></p>
 </template>
 
-<script setup>
+<script setup lang="ts">
 definePageMeta({
   layout: 'login' 
+})
+
+import { onMounted, onBeforeUnmount  } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter();
+
+let intervalID: ReturnType<typeof setInterval> | null = null;
+
+onMounted(() => {
+  intervalID = setInterval(() => {
+    router.push('/')
+  }, 3000)
+})
+
+onBeforeUnmount(() => {
+  if (intervalID) clearInterval(intervalID)
 })
 </script>
