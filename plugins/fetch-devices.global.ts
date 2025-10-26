8cfb4dd33df321c7e218cc66961884d4b3f5bfa0
whiteAndBlackPanda <142206766+camachoRamel@users.noreply.github.com>
2025-10-26 21:50:22 +0800

    updated fetch devices global

--- plugins/fetch-devices.global.ts
+++ plugins/fetch-devices.global.ts
@@ -1,14 +1,17 @@
 import { useFloodMessageStore } from '~/app/stores/useFloodMessageStore';
 import { useLocationStore } from '~/app/stores/useLocationStore';
 import { useRecordsStore } from '~/app/stores/useRecordsStore';
+import { useWaterLevelStore } from '~/app/stores/useWaterLevelStore';
 
 export default defineNuxtPlugin(async () => {
   const locationStore = useLocationStore();
   const floodMessageStore = useFloodMessageStore();
   const recordsStore = useRecordsStore();
+  const waterLevelLogsStore = useWaterLevelStore();
   await locationStore.fetchDevicesLazy();
   await floodMessageStore.fetchMessages();
   await recordsStore.fetchActionLogs();
+  await waterLevelLogsStore.fetchWaterLevelLogs();
   floodMessageStore.setFloodMessage(floodMessageStore.floodMessages[0])
   locationStore.setDevice(locationStore.devices[0])
 })
